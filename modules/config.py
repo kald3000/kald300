d9aad8d1617ec10d48bbd14a62b5e32c07eeb074
Aditya <94355314+MrAdityaXD@users.noreply.github.com>
2022-07-01 13:10:52 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -6,12 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 que = {}
 admins = {}
+aiohttpsession = aiohttp.ClientSession()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_TOKEN = getenv("BOT_TOKEN")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "300"))
-STRING_SESSION = getenv("STRING_SESSION")
+STRING_SESSION = getenv("STRING_SESSION", "")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1282754256").split()))
-aiohttpsession = aiohttp.ClientSession()
